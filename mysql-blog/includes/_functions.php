255a5760c86b4b9ec66d0cecb91224a615d8b8ec
Nicholas Peters <nicholas.d.peters@gmail.com>
2019-10-28 00:10:24 -0600

    minor change to link function

--- mysql-blog/includes/_functions.php
+++ mysql-blog/includes/_functions.php
@@ -25,17 +25,17 @@ function makeClickableLinks($text)
 
   $text = preg_replace(
     '/(((f|ht){1}tps?:\/\/)[-a-zA-Z0-9@:%_\+.~#?&\/\/=]+)/i',
-    '<a href="\\1">\\1</a>',
+    '<a target="_blank" href="\\1">\\1</a>',
     $text
   );
   $text = preg_replace(
     '/([[:space:]()[{}])(www.[-a-zA-Z0-9@:%_\+.~#?&\/\/=]+)/i',
-    '\\1<a href="http://\\2">\\2</a>',
+    '\\1<a target="_blank" href="http://\\2">\\2</a>',
     $text
   );
   $text = preg_replace(
     '/([_\.0-9a-z-]+@([0-9a-z][0-9a-z-]+\.)+[a-z]{2,3})/i',
-    '<a href="mailto:\\1">\\1</a>',
+    '<a target="_blank" href="mailto:\\1">\\1</a>',
     $text
   );
   return trim($text);
