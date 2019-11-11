8873983c623276b115d0b56b22653cc545f463f1
Nicholas Peters <nicholas.d.peters@gmail.com>
2019-11-11 15:57:58 -0700

    added card style display

--- image-gallery/index.php
+++ image-gallery/index.php
@@ -13,16 +13,43 @@ include("includes/header.php");
 $result = mysqli_query($con, "SELECT * FROM image_gallery");
 ?>
 
-<?php while ($row = mysqli_fetch_array($result)) : ?>
-  <div class="thumb">
-    <div class="image">
-      <a href="gallery.php?id=<?php echo $row['id']; ?>">
-        <img src="images/squares/<?php echo $row['npe_file']; ?>" alt="thumbnail" />
-      </a>
-    </div>
-  </div>
-<?php endwhile; ?>
+<style>
+  .gallery-card-container {
+    margin: 0 auto;
+    max-width: 960px;
+    padding-left: 1.1rem;
+  }
 
+  .gallery-card {
+    padding-top: .7rem;
+    height: 240px;
+    background: black;
+    border: 2px solid black;
+    border-radius: 6px;
+    margin-right: 1.1rem;
+    margin-bottom: 1rem;
+  }
+
+  .gallery-card-image {
+    padding-top: .7rem;
+    padding-left: .5rem;
+    padding-right: .5rem;
+    background: white;
+    height: 95%;
+  }
+</style>
+
+<div class="row d-flex gallery-card-container">
+  <?php while ($row = mysqli_fetch_array($result)) : ?>
+    <a href="gallery.php?id=<?php echo $row['id']; ?>">
+    <div class="gallery-card">
+      <div class="gallery-card-image">
+          <img src="images/squares/<?php echo $row['npe_file']; ?>" alt="thumbnail" />
+        </div>
+      </div>
+    </a>
+  <?php endwhile; ?>
+</div>
 
 
 
