dc554ea7b6dba60fc30fc0fcacdd132a71fe4ce7
Nicholas Peters <nicholas.d.peters@gmail.com>
2019-11-11 17:54:37 -0700

    gallery display working // page forward and back function added

--- image-gallery/gallery.php
+++ image-gallery/gallery.php
@@ -6,30 +6,63 @@ include("includes/header.php");
 $id = $_GET['id'];
 ?>
 
-
 <?php
 $result = mysqli_query($con, "SELECT * FROM image_gallery WHERE id = $id");
 ?>
 
 <?php while ($row = mysqli_fetch_array($result)) : ?>
-  <!-- Go ahead and do some HTML/CSS styling in here...I dare you! -->
-  <div class="jumbotron clearfix">
-    <h1><?php echo $row['npe_business_name']; ?></h1>
-  </div>
+    <!-- Go ahead and do some HTML/CSS styling in here...I dare you! -->
+    <div class="row">
+        <div class="col-9">
+            <?php if ($row['npe_file'] != "") : ?>
+                <img src="images/display/<?php echo $row['npe_file']; ?>" alt="">
+            <?php endif; ?>
+        </div>
 
-  <?php if ($row['npe_title'] != "") : ?>
-      <h3><?php echo $row['npe_title']; ?></h3>
-  <?php endif; ?>
-  
-  <?php if ($row['npe_file'] != "") : ?>
-      <img src="images/display/<?php echo $row['npe_file']; ?>" alt="">
-  <?php endif; ?>
-  
-  <?php if ($row['npe_description'] != "") : ?>
-      <h3><?php echo $row['npe_description']; ?></h3>
-  <?php endif; ?>
+        <div class="col-3">
+            <?php if ($row['npe_title'] != "") : ?>
+                <h1><?php echo $row['npe_title']; ?></h1>
+            <?php endif; ?>
+            <?php if ($row['npe_description'] != "") : ?>
+                <p><?php echo $row['npe_description']; ?></p>
+            <?php endif; ?>
+            <div>
+                <button class="btn btn-info back-button"><<-</button>
+                <button class="btn btn-info next-button">->></button>
+            </div>
+        </div>
+    </div>
 
 <?php endwhile; ?>
+
+<script type="text/javascript">
+    nextButton = document.querySelector(".next-button");
+    backButton = document.querySelector(".back-button");
+
+    nextButton.addEventListener("click", e => {
+        <?php
+        $nextPage = $id + 1;
+        $result = mysqli_query($con, "SELECT * FROM image_gallery WHERE id > $id LIMIT 1");
+        ?>
+        <?php while ($row = mysqli_fetch_array($result)) : ?>
+            <?php if ($row['id'] != null) : ?>
+                window.location.href = "http://npeters5.dmitstudent.ca/dmit2025/image-gallery/gallery.php?id=<?php echo $row['id'] ?>";
+            <?php endif; ?>
+        <?php endwhile; ?>
+    })
+
+    backButton.addEventListener("click", e => {
+        <?php
+        $nextPage = $id + 1;
+        $result = mysqli_query($con, "SELECT * FROM image_gallery WHERE id < $id LIMIT 1");
+        ?>
+        <?php while ($row = mysqli_fetch_array($result)) : ?>
+            <?php if ($row['id'] != null) : ?>
+                window.location.href = "http://npeters5.dmitstudent.ca/dmit2025/image-gallery/gallery.php?id=<?php echo $row['id'] ?>";
+            <?php endif; ?>
+        <?php endwhile; ?>
+    })
+</script>
 
 <?php
 include("includes/footer.php");
